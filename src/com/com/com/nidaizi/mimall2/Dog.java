c37ea79f9078e2c26df50607ba5f4cdfcbb1fd9f
big <mrkeep@163.com>
2018-10-19 17:03:17 +0800

    新增類別

--- src/com/com/com/nidaizi/mimall2/Dog.java
+++ src/com/com/com/nidaizi/mimall2/Dog.java
@@ -1,6 +1,22 @@
 package com.com.nidaizi.mimall2;
 
 public class Dog {
-    public  int age;
+        private  int age;
         private String like;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLike() {
+        return like;
+    }
+
+    public void setLike(String like) {
+        this.like = like;
+    }
 }
