906679a585ebd88bb28a3ab2e15b096d3b0eb151
big <mrkeep@163.com>
2018-10-31 14:52:29 +0800

    同一个作用域不能声明两个一样名字的变量

---
+++ src/com/com/dataType/SimpleExample006.java
@@ -0,0 +1,10 @@
+package com.dataType;
+
+public class SimpleExample006 {
+    //同一作用域不能声明两个名字相同的变量,即名字不能重复
+    String a(){
+        String q;
+//        String q; //报错已经定义过了,不能在写同名的变量;
+        return "";
+    };
+}
