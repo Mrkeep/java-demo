7e137c335c2cb102774b1bce12847a534931174a
albert <mrkeep@163.com>
2018-12-12 15:04:23 +0800

    解决包冲突问题,新增if嵌套小案例

---
+++ src/com/Example/For.java
@@ -0,0 +1,35 @@
+package com.Example;
+
+public class For {
+
+    public static void main(String[] args) {
+        String a = "我是友人";
+        if (a.equals("我是友人")){
+            if (a.equals("我是友人") && a.equals("北京")){
+                System.out.println("Welcome to China!");
+            }else if (a.equals("我是友人")){
+                System.out.println("不欢迎");
+            }
+        }
+
+
+        int b = 1;
+        if (b==1){
+            if (b>0){
+                if (b<2){
+                    if (b>0.32323){
+                        if(b != 996){
+                            System.out.println("\n");
+                            System.out.println("这周不用加班!");
+                        }
+                    }
+                }
+            }
+        };
+
+    }
+
+
+
+
+}
